bb34a127d83da02f86164b44352a6dc407204fff
beregovoy68 <s.a.ishchenko@gmail.com>
2018-02-28 21:59:21 +1100

    Removed unused service. Added a constant for mass payment transactions.

--- distr/wavesplatform-core.js
+++ distr/wavesplatform-core.js
@@ -2966,7 +2966,7 @@ Decimal.config({toExpNeg: -(Currency.WAVES.precision + 1)});
             START_LEASING_TRANSACTION_TYPE: 8,
             CANCEL_LEASING_TRANSACTION_TYPE: 9,
             CREATE_ALIAS_TRANSACTION_TYPE: 10,
-            MAKE_ASSET_NAME_UNIQUE_TRANSACTION_TYPE: 11
+            MASS_PAYMENT_TRANSACTION_TYPE: 11
         });
 })();
 
@@ -3717,59 +3717,6 @@ Decimal.config({toExpNeg: -(Currency.WAVES.precision + 1)});
     angular
         .module('waves.core.services')
         .service('aliasRequestService', AliasRequestService);
-})();
-
-(function () {
-    'use strict';
-
-    function UniqueAssetsRequestService (constants, utilityService, cryptoService,
-                                         validateService) {
-
-        function buildSignature(bytes, sender) {
-            var privateKeyBytes = cryptoService.base58.decode(sender.privateKey);
-            return cryptoService.nonDeterministicSign(privateKeyBytes, bytes);
-        }
-
-        function buildMakeAssetUniqueSignatureData(networkByte, senderPublicKey, assetId, fee, timestamp) {
-            var typeByte = [constants.MAKE_ASSET_NAME_UNIQUE_TRANSACTION_TYPE];
-
-            var publicKeyBytes = utilityService.base58StringToByteArray(senderPublicKey);
-            var assetIdBytes = utilityService.base58StringToByteArray(assetId);
-            var feeBytes = utilityService.longToByteArray(fee);
-            var timestampBytes = utilityService.longToByteArray(timestamp);
-
-            return [].concat(typeByte, networkByte, publicKeyBytes, assetIdBytes, feeBytes, timestampBytes);
-        }
-
-        this.buildMakeAssetNameUniqueRequest = function (asset, sender) {
-            validateService.validateSender(sender);
-
-            var networkByte = utilityService.getNetworkIdByte();
-
-            var assetId = asset.assetId;
-            var fee = asset.fee.toCoins();
-            var time = asset.time || utilityService.getTime();
-
-            var signatureData = buildMakeAssetUniqueSignatureData(networkByte, sender.publicKey, assetId, fee, time);
-            var signature = buildSignature(signatureData, sender);
-
-            return {
-                senderPublicKey: sender.publicKey,
-                assetId: assetId,
-                fee: fee,
-                networkByte: networkByte,
-                timestamp: time,
-                signature: signature
-            };
-        };
-    }
-
-    UniqueAssetsRequestService.$inject = ['constants.transactions', 'utilityService', 'cryptoService',
-                                          'validateService'];
-
-    angular
-        .module('waves.core.services')
-        .service('uniqueAssetsRequestService', UniqueAssetsRequestService);
 })();
 
 (function () {
